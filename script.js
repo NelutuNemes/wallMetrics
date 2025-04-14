91e4ca14b576b66148ea347c27f985ccaa663666
Nelutu Nemes <nelutunemes10@gmail.com>
2025-04-14 20:39:37 +0300

    fix css reset function

--- script.js
+++ script.js
@@ -371,6 +371,7 @@ function resetAll() {
     priceResultBody.innerHTML = ``;
     document.body.classList.remove("withoutBackgroundImage");
     document.body.classList.add("withBackgroundImage");
+    // test
 
 
 }
